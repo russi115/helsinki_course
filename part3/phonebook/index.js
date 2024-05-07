7653967bada8c9c4c9c7ff183c0657365b6712b1
russi115 <shamanse2001@gmail.com>
2024-05-07 16:50:12 -0400

    exercise 3.8 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,25 @@ var morgan = require('morgan')
 const app = express()
 app.use(express.json())
 
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    if(req.method == 'POST'){
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            JSON.stringify(req.body)
+        ].join(' ')
+    }
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+    ].join(' ')
+}))
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })

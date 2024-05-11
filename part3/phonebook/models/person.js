2ebb81a066f34d4839bd8db0cb6612973bdc7c07
russi115 <shamanse2001@gmail.com>
2024-05-11 18:12:26 -0400

    exercise 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -22,9 +22,15 @@ const personSchema = new mongoose.Schema({
       required: true
     },
     number: {
-      type: Number,
-      minlength: 3,
-      required: true
+      type: String,
+      minlength: 8,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{6,9}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
     },
 })
 

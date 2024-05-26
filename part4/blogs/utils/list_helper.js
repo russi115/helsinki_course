9fd1c76b0faec0601f1a4a22fa3f6f3b639ff1af
russi115 <shamanse2001@gmail.com>
2024-05-26 00:24:52 -0400

    exercise 4.6 done with lodash

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,62 +1,61 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
 
-const totalLikes = (blogs) => { 
-    if(blogs.length == 0){
-        return 0
-    }else{
-        let cont = 0
-        blogs.forEach(element => {
-            cont += element.likes
-        });
-        return cont
+const totalLikes = (blogs) => {
+  if (blogs.length == 0) {
+    return 0;
+  } else {
+    let cont = 0;
+    blogs.forEach((element) => {
+      cont += element.likes;
+    });
+    return cont;
+  }
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  let favorite = blogs[0];
+
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > favorite.likes) {
+      favorite = blogs[i];
     }
-}
+  }
 
-const favoriteBlog  = (blogs) => { 
-    if (blogs.length === 0) {
-        return 0;
-    }
-
-    let favorite = blogs[0];
-
-    for (let i = 1; i < blogs.length; i++) {
-      if (blogs[i].likes > favorite.likes) {
-        favorite = blogs[i];
-      }
-    }
-  
-    return {
-      title: favorite.title,
-      author: favorite.author,
-      likes: favorite.likes
-    };
-}
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) {
-      return 0;
-    }
-  
-    let blogCounts = {};
-  
-    blogs.forEach(blog => {
-      if (blog.author in blogCounts) {
-        blogCounts[blog.author]++;
-      } else {
-        blogCounts[blog.author] = 1;
-      }
-    });
-  
-    let maxBlogsAuthor = Object.keys(blogCounts).reduce((a, b) => blogCounts[a] > blogCounts[b] ? a : b);
-  
-    return {
-      author: maxBlogsAuthor,
-      blogs: blogCounts[maxBlogsAuthor]
-    };
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  const blogCounts = _.countBy(blogs, "author");
+  const maxBlogsAuthor = _.maxBy(
+    _.keys(blogCounts),
+    (author) => blogCounts[author]
+  );
+
+  return {
+    author: maxBlogsAuthor,
+    blogs: blogCounts[maxBlogsAuthor],
   };
-  
+};
+
 module.exports = {
-dummy, totalLikes, favoriteBlog, mostBlogs
-}
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+};
